296653a8a64fc5920754a49b40321f28bed5208e
Mizraim Martínez <mizraim.martinez@gmail.com>
2023-02-09 19:54:44 -0600

    add structure basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './componets/toolbar/toolbar.component';
+import { NavbarComponent } from './componets/navbar/navbar.component';
+import { ContentComponent } from './componets/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
